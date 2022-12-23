9caea03540e5c345b8ce35cb2a42d2d988e8d947
biggucci1017 <omarfarouq107@gmail.com>
2022-12-22 19:52:08 -0600

    Got buttons to work.. working on displaying results and score.

---
+++ script.js
@@ -0,0 +1,72 @@
+let compChoice = Math.random();
+
+function getComputerChoice() {
+    return ['rock','paper','scissors'][~~(Math.random()*3)];
+}
+getComputerChoice();
+let gameResult = "tie";
+let winner;
+
+function displaytie() {
+    document.getElementById("result").innerHTML = "The result is a tie."
+}
+
+function displaywin() {
+    document.getElementById("result").innerHTML = "Congrats! You win this round."
+}
+
+function displayloss() {
+    document.getElementById("result").innerHTML = "Sorry... you lose this round."
+}
+
+document.getElementById("rock").addEventListener("click", rockRound);
+function rockRound(e)
+{
+    let compChoice = getComputerChoice();
+    if (compChoice === "rock") {
+        displaytie();
+        }
+    else if (compChoice === "scissors"){
+        gameResult = "User wins! Rock beats scissors.";
+        displaywin();
+        }
+    else if (compChoice === "paper") {
+        gameResult = "User lost. Paper beats rock.";
+        displayloss();
+        }
+}
+
+document.getElementById("paper").addEventListener("click", paperRound);
+function paperRound(e) 
+{
+    let compChoice = getComputerChoice();
+    if (compChoice === "paper") {
+        displaytie();
+    }
+    if (compChoice === "scissors") {
+        gameResult = "User lost. Scissors beats paper."
+        displayloss();
+    }
+    if (compChoice === "rock") {
+        gameResult = "User wins! Paper beats rock."
+        displaywin();
+    }
+}
+
+document.getElementById("scissors").addEventListener("click", scissorsRound);
+function scissorsRound(e)
+{
+    let compChoice = getComputerChoice();
+    if (compChoice === "scissors") {
+        displaytie();
+    }
+    if (compChoice === "rock") {
+        gameResult = "User lost. Rock beats scissors."
+        displayloss();
+    }
+    if (compChoice === "paper") {
+        gameResult = "User wins! Paper beats rock."
+        displaywin();
+    }
+
+}
